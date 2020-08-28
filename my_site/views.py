01c6633653c73cc2751372ccff6fcc8514692768
User <y.kuzmenko740@gmail.com>
2020-08-28 14:01:42 +0300

    Minor improvements

--- my_site/views.py
+++ my_site/views.py
@@ -3,30 +3,35 @@ import requests
 from bs4 import BeautifulSoup
 from . import models
 
-BASE_OLX_URL = 'https://www.olx.ua/uk/{}/q-{}/'
-CITIES = ['Kiev', 'Dnepr', 'Kharkov', 'Odessa', 'Lvov']
+BASE_OLX_URL = 'https://www.olx.ua/uk/{list}/q-{item}/?page={page}'
+HEADERS = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36 OPR/67.0.3575.130",
+    "accept": "*/*"}
+CITIES = ['kiev', 'dnepr', 'kharkov', 'odessa', 'lvov']
 
 
-def home(request):
-    return render(request, template_name='base.html')
-
-
-def new_search(request):
-    search = request.POST.get('search')
-    city = request.POST.get('city')
-    models.Search.objects.create(search=search,city=city)
-    if city in CITIES:
-        final_url = BASE_OLX_URL.format(city,search.replace(" ", "-"))
+def get_pages_count(URL):
+    html = get_html(URL)
+    soup = BeautifulSoup(html, "html.parser")
+    pagination = soup.find_all("span", class_="item fleft")
+    if pagination:
+        return int(pagination[-1].get_text())
     else:
-        final_url = BASE_OLX_URL.format('list',search.replace(" ", "-"))
-    print(final_url)
-    response = requests.get(final_url)
-    data = response.text
+        return 1
 
+
+def get_html(url, params=None):
+    r = requests.get(url, headers=HEADERS, params=params)
+    return r
+
+
+def get_content(html):
+    data = html.text
     soup = BeautifulSoup(data, 'html.parser')
     tabs = soup.find_all('tr', 'wrap')
 
-    final_tabs = []
+    tabs_res = []
+
     for tab in tabs:
         tabs_title = tab.find('a', 'marginright5 link linkWithHash detailsLink').text.strip()
         tab_url = tab.find('a').get('href')
@@ -37,9 +42,61 @@ def new_search(request):
         else:
             tab_img_url = "https://748073e22e8db794416a-cc51ef6b37841580002827d4d94d19b6.ssl.cf3.rackcdn.com/not-found.png"
 
-        final_tabs.append((tabs_title, tab_url, tab_price, tab_date_city, tab_img_url))
+        tabs_res.append((tabs_title, tab_url, tab_price, tab_date_city, tab_img_url))
+    return tabs_res
+
+
+def home(request):
+    return render(request, template_name='base.html')
+
+
+def new_search(request):
+    search = request.POST.get('search')
+    city = request.POST.get('city')
+    models.Search.objects.create(search=search, city=city)
+    city = city.lower()
+    if city not in CITIES:
+        city = 'list'
+    final_url = BASE_OLX_URL.format(list=city, item=search.replace(" ", "-"), page=1)
+    html = get_html(final_url)
+    final_tabs = []
+    final_tabs.extend(get_content(html))
+
     request_for_frontend = {
         'search': search,
         'final_tabs': final_tabs,
     }
     return render(request, 'my_site/new_search.html', context=request_for_frontend)
+
+
+""" 
+    INVALID SCHEMA
+    *NOT WORKING*
+ """
+# def new_search(request):
+#     search = request.POST.get('search')
+#     city = request.POST.get('city')
+#     models.Search.objects.create(search=search, city=city)
+#     city = city.lower()
+#     if city not in CITIES:
+#         city = 'list'
+#     final_url = BASE_OLX_URL.format(list=city, item=search.replace(" ", "-"), page=1)
+#     html = get_html(final_url)
+#     final_tabs = []
+#     if html.status_code == 200:
+#
+#         pages_count = get_pages_count(html.text)
+#         for page in range(1, pages_count + 1):
+#             print(f'Парсинг страницы: {page} из {pages_count} . . .')
+#             final_url = BASE_OLX_URL.format(list=city, item=search.replace(" ", "-"), page=page)
+#             html = get_html(final_url)
+#             final_tabs.extend(get_content(html))
+#     else:
+#         print("Error")
+#
+#
+#     request_for_frontend = {
+#         'search': search,
+#         'final_tabs': final_tabs,
+#     }
+#     return render(request, 'my_site/new_search.html', context=request_for_frontend)
--- search_engine/settings.py
+++ search_engine/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -27,7 +27,6 @@ SECRET_KEY = 'uftk7q1ayf(^y2h98o6z@hf94l4@*4k-+3=^=r%+l*a)&h(ote'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'search_engine.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -81,7 +79,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -101,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -115,12 +111,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT =os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [
-    'static/css',
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
